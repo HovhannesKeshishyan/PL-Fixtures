d1c20e833b8a82e3609a2fe948fdd7e311f2c1c2
hovhannesk <hovhannes.keshishyan.external@worldline.com>
2025-02-13 11:37:43 +0400

    change today matches popover label to "Today"

--- src/helpers/date-parse.ts
+++ src/helpers/date-parse.ts
@@ -1,17 +1,35 @@
-export const utcDateToLocal = (utcDate: string): [string, string] => {
+import type {DateTimeFormatOptions} from "@/types/types";
+
+
+const dateFormat: Readonly<DateTimeFormatOptions> = {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
+}
+
+const timeFormat: Readonly<DateTimeFormatOptions> = {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false
+}
+
+const isTodayDate = (localDate: Date, today: Date): boolean => {
+    return localDate.getFullYear() === today.getFullYear() &&
+        localDate.getMonth() === today.getMonth() &&
+        localDate.getDate() === today.getDate();
+}
+
+export const utcDateToLocal = (utcDate: string, todayLabel: string | false = "Today"): [string, string] => {
     const localDate = new Date(utcDate);
+    const today = new Date();
 
-    const formattedDate = localDate.toLocaleDateString('en-GB', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric'
-    });
+    let formattedDate = localDate.toLocaleDateString('en-GB', dateFormat);
 
-    const formattedTime = localDate.toLocaleTimeString('en-GB', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-    });
+    const formattedTime = localDate.toLocaleTimeString('en-GB', timeFormat);
+
+    if (todayLabel && isTodayDate(localDate, today)) {
+        formattedDate = todayLabel
+    }
 
     return [formattedDate, formattedTime];
 }
